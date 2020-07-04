78aa0646b7ad01c507ec1a88054c7a1e8cfadd16
Tianyu20809 <1365252288@163.com>
2020-07-05 00:49:03 +0800

    新增sheetName参数

--- readExcel.js
+++ readExcel.js
@@ -22,7 +22,7 @@ function _readFile(file) {
  * 读取文件信息，把读取到的文件转化成服务器可以解析的json数据
  * file是所需读取的excel文件
  * _keyMapRule是提前定义好的映射转换规则
- * 
+ * sheetName是sheet页名称, 如果不传递，则默认读取第一个sheet页的名称
  * _keyMap示例：（如：name对应excel文件中的姓名列，phone对应excel文件中的电话列）
  * 
         // {
@@ -39,16 +39,17 @@ function _readFile(file) {
  * 
  */
 
-function Excle2Json(file, _keyMapRule) {
+function Excle2Json(file, _keyMapRule, sheetName) {
     return new Promise(function(resolve, reject) {
         _readFile(file).then(function(data) {
             if (window.XLSX) {
                 var xlsx = window.XLSX;
             } else {
-                reject("没有引入xlsx类库，请到官网下载并引入:https://github.com/SheetJS/sheetjs/blob/master/dist/xlsx.core.min.js")
+                reject("没有引入js-xlsx库，请到官网下载并引入:https://github.com/SheetJS/sheetjs/blob/master/dist/xlsx.core.min.js")
             }
             var workbook = xlsx.read(data, { type: 'buffer' }),
-                worksheet = workbook.Sheets[workbook.SheetNames[0]];
+                sheetName = sheetName ? sheetName : workbook.SheetNames[0];
+            worksheet = workbook.Sheets[sheetName]; //默认读取第一个sheet页的数据
             //此时获取到的数据是data是原生的数据，如果excel是中文的话，我们转换出来的json数据的属性名也是中文，所以需要将其属性名转化为英文
             data = xlsx.utils.sheet_to_json(worksheet);
             //转化属性名
