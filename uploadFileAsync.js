c4e89060413cbed928b979cc6a0549b71b3f6e87
Tianyu20809 <1365252288@163.com>
2020-08-04 12:10:00 +0800

    修改附件上传的代码，重新整理上传逻辑

--- uploadFileAsync.js
+++ uploadFileAsync.js
@@ -10,10 +10,10 @@
 
 /**
  * 
- * @param {(enent obj)}  * kendoUI的组件的事件对象 或者 传递file控件的id值 
+ * @param {(event obj)}  * kendoUI的组件的事件对象 或者 传递file控件的id值 
  */
 function onUploadFiles(e) {
-    //默认传入对象是kendoUI的时间对象
+    //默认传入对象是kendoUI的事件对象
     if (typeof e === 'object') {
         //此时默认为kendoui的事件对象
         var files = e.files;
@@ -22,14 +22,14 @@ function onUploadFiles(e) {
          * 此处请设置 listName, listItemID的参数值
          */
 
-        _uploadFilesCommonAsync(files, listName, listItemID).then(function(message) {
+        _uploadFilesCommonAsync(files, listName, listItemID).then(function (message) {
             console.log(message);
-        }).catch(function(e) {
+        }).catch(function (e) {
             console.log(e)
         })
     } else if (typeof e === 'string') {
         //获取上传控件id，根据id获取目前上传的附件
-        var files = $("#" + e)[0].files[0]; //获取所上传的文件数组
+        var files = $("#" + e)[0].files; //获取所上传的文件类数组对象
         var listName, listItemID;
         /**
          * 此处请设置 listName, listItemID的参数值
@@ -37,9 +37,17 @@ function onUploadFiles(e) {
 
         var length = files.length; //文件数组长度
         if (length === 0) return; //说明没有上传新的文件
-        _uploadFilesCommonAsync(files, listName, listItemID).then(function(message) {
-            console.log(message);
-        }).catch(function(e) {
+
+        //执行上传方法
+        _uploadFilesCommonAsync(files, listName, listItemID).then(function (message) {
+            /**
+             * 此处可以处理返回信息弹窗或是取消遮罩层的操作
+             */
+            console.log(message); //打印信息
+        }).catch(function (e) {
+            /**
+             * 此处可以处理返回信息弹窗或是取消遮罩层的操作
+             */
             console.log(e)
         })
     } else {
@@ -50,29 +58,38 @@ function onUploadFiles(e) {
 /**
  * 
  * @param files 在页面上上传的多个文件
- * @param listName  目标form的名称
- * @param listItemID form item id
+ * @param listName  sharepoint list的名称
+ * @param listItemID list item id
  */
 function _uploadFilesCommonAsync(files, listName, listItemID) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
         //如果必填参数没有填写，返回false
         if (!files || !listName || !listItemID) {
             return Promise.reject(false);
         }
-        Promise.all(files.map(function(file) {
-            return new Promise(function(resolve1, reject1) {
+        var f = isArray(files); //files是数组返回true，不是数组返回false 
+        if (!f) {
+            //不是数组
+            files = Array.prototype.slice.call(files); //将类数组对象转化为数组
+        }
+        Promise.all(files.map(function (file) {
+            return new Promise(function (resolve1, reject1) {
                 var reader = new FileReader();
-                file = file.rawFile;
+                /**
+                 * 如果使用了KendoUI插件
+                 * 则需要从file中的rawFile提取文件属性
+                 */
+                file.rawFile ? file = file.rawFile : null;
                 var fileName = file.name;
                 var obj = {};
                 //当文件读取成功之后
-                reader.onloadend = function(event) {
+                reader.onloadend = function (event) {
                     obj.result = event.target.result;
                     obj.fileName = fileName;
                     resolve1(obj);
                 };
                 //当文件读取失败之后
-                reader.onerror = function(event) {
+                reader.onerror = function (event) {
                     obj.result = event.target.result;
                     obj.fileName = fileName;
                     reject1(obj);
@@ -80,58 +97,114 @@ function _uploadFilesCommonAsync(files, listName, listItemID) {
                 //读取文件的blob内容
                 reader.readAsArrayBuffer(file);
 
-            }).then(function(data) {
-                //判断，并执行上传
-                return new Promise(function(resolve2, reject2) {
-                    //SP对象是SharePoint环境变量（全局）, 在SharePoint环境下存在
+            })
+        }))
+            .then(function (filesCollect) {
+                //filesCollect.result是解析完成file文件集合
+                //len是所上传的文件数量
+                var len = filesCollect.length;
+                //所上传的文件名集合容器
+                var filesNameContainer = [];
+                return new Promise(function (resolve) {
                     if (!window.SP.Base64EncodedByteArray) {
                         //如果没有加载sharepoint相应类库，先加载类库方法，然后执行上传逻辑
-                        SP.SOD.executeFunc("sp.js", "SP.ClientContext", function() {
-                            var contentData = transformBlob(data.result);
-                            uploadFileToSPServer(listName, listItemID, data.fileName, contentData).then(function(b) {
-                                if (b) {
-                                    //当前文件上传成功
-                                    var obj = {};
-                                    var fileName = data.fileName;
-                                    var status = 'success';
-                                    obj.fileName = fileName;
-                                    obj.status = status;
-                                    resolve2(obj);
-                                } else {
-                                    //当前文件上传失败
-                                    var obj = {};
-                                    var fileName = data.fileName;
-                                    var status = 'error';
-                                    obj.fileName = fileName;
-                                    obj.status = status;
-                                    resolve(obj);
-                                }
-                            })
-                        });
-                    } else {
-                        //已经加载了内置类库，执行上传逻辑
-                        var contentData = transformBlob(data.result);
-                        uploadFileToSPServer(listName, listItemID, data.fileName, contentData).then(function(b) {
-                            if (b) {
-                                //当前文件上传成功
-                                resolve2(true)
-                            } else {
-                                //当前文件上传失败
-                                reject2(false)
-                            }
+                        SP.SOD.executeFunc("sp.js", "SP.ClientContext", function () {
+                            //声明一个递归函数, 使用递归的方式进行附件上传
+                            (function loopCallBack(index) {
+                                var contentData = transformBlob(filesCollect[index].result);
+                                contentData = contentData.toBase64String();
+                                $().SPServices({
+                                    operation: "AddAttachment",
+                                    listName: listName,
+                                    async: true,
+                                    listItemID: listItemID,
+                                    fileName: fileName,
+                                    attachment: contentData,
+                                    completefunc: function (xData, Status) {
+                                        if (Status == 'success') {
+                                            filesNameContainer.push(filesCollect[index].fileName); //将上传成功的文件的文件名放入容器中
+                                            if (index < len - 1) {
+                                                //条件成立，还需要继续上传下一个文件
+                                                index = index + 1;
+                                                loopCallBack(i);
+                                            } else {
+                                                //后面没有新的文件要上传了, 将所有上传成功的文件的文件名输出
+                                                resolve(filesNameContainer);
+                                            }
+
+                                        } else {
+                                            //没有成功的不放入容器数组中
+                                            if (index < len - 1) {
+                                                //条件成立，还需要继续上传下一个文件
+                                                index = index + 1;
+                                                loopCallBack(i);
+                                            } else {
+                                                //后面没有新的文件要上传了, 将所有上传成功的文件的文件名输出
+                                                resolve(filesNameContainer)
+                                            }
+                                        }
+                                    }
+                                });
+                            })(0)
                         })
+                    } else {
+                        //声明一个递归函数, 使用递归的方式进行附件上传
+                        (function loopCallBack(index) {
+                            var contentData = transformBlob(filesCollect[index].result);//转化每一个文件为blob格式
+                            contentData = contentData.toBase64String();
+                            $().SPServices({
+                                operation: "AddAttachment",
+                                listName: listName,
+                                async: true,
+                                listItemID: listItemID,
+                                fileName: fileName,
+                                attachment: contentData,
+                                completefunc: function (xData, Status) {
+                                    if (Status == 'success') {
+                                        //将上传成功的文件的文件名放入容器中
+                                        var obj = {};
+                                        obj.status = 'success';
+                                        obj.fileName = filesCollect[index].fileName;
+                                        filesNameContainer.push(obj);
+                                        if (index < len - 1) {
+                                            //条件成立，还需要继续上传下一个文件
+                                            index = index + 1;
+                                            loopCallBack(i);
+                                        } else {
+                                            //后面没有新的文件要上传了, 将所有上传成功的文件的文件名/状态 输出
+                                            resolve(filesNameContainer);
+                                        }
+                                    } else {
+                                        //没有成功的不放入容器数组中
+                                        var obj = {};
+                                        obj.status = 'error';
+                                        obj.fileName = filesCollect[index].fileName;
+                                        filesNameContainer.push(obj);
+                                        if (index < len - 1) {
+                                            //条件成立，还需要继续上传下一个文件
+                                            index = index + 1;
+                                            loopCallBack(i);
+                                        } else {
+                                            //后面没有新的文件要上传了, 将所有上传的文件的文件名/状态 输出
+                                            resolve(filesNameContainer)
+                                        }
+                                    }
+                                }
+                            });
+                        })(0)
                     }
                 })
             })
-        })).then(function(result) {
-            //此时所有附件上传完成
-            console.log(result); //result里同时存在成功上传的附件和失败上传的附件信息
-            resolve(result);
-        }).catch(function(e) {
-            //有的附件没有上传完成
-            console.log(e);
-            reject('文件上传失败');
-        })
+            .then(function (result) {
+                //此时所有附件上传完成
+                console.log(result); //result为对象数组，每个元素是每个文件上传的状态
+                resolve(result);
+            })
+            .catch(function (e) {
+                //有的附件没有上传完成
+                console.log(e);
+                reject('文件上传失败');
+            })
 
     })
 }
@@ -151,27 +224,39 @@ function transformBlob(buffer) {
 }
 
 /**
- * 
+ * 未使用
  * @param listName 列表名称
  * @param listItemID  表单ID编号
  * @param contentData  附件数据（buffer）
  */
-function uploadFileToSPServer(listName, listItemID, fileName, contentData) {
-    return new Promise(function(resolve, reject) {
-        $().SPServices({
-            operation: "AddAttachment",
-            listName: listName,
-            async: true,
-            listItemID: listItemID,
-            fileName: fileName,
-            attachment: contentData.toBase64String(),
-            completefunc: function(xData, Status) {
-                if (Status != 'success') {
-                    resolve(true);
-                } else {
-                    reject(false);
-                }
-            }
-        });
-    })
+
+
+// function uploadFileToSPServer(listName, listItemID, fileName, contentData) {
+//     return new Promise(function (resolve, reject) {
+//         $().SPServices({
+//             operation: "AddAttachment",
+//             listName: listName,
+//             async: true,
+//             listItemID: listItemID,
+//             fileName: fileName,
+//             attachment: contentData.toBase64String(),
+//             completefunc: function (xData, Status) {
+//                 if (Status != 'success') {
+//                     resolve(true);
+//                 } else {
+//                     reject(false);
+//                 }
+//             }
+//         });
+//     })
+// }
+
+/**
+ * 判断一个变量是否为数组
+ * 是数组返回true
+ * 非数组返回false
+ * @param {*} obj 
+ */
+function isArray(obj) {
+    return Object.prototype.toString.call(obj) === '[object Array]';
 }
